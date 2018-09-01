fdaecb5033f54db9afbe4f1ad7c1a67f28fbf2f3
zhouzhaoyan <zhouzhaoyan@lifesense.com>
2018-09-01 11:43:22 +0800

    更新代码

---
+++ com.yan.hot.legend/app/src/main/java/com/example/com/yan/hot/legend/ChangeCoordinate.java
@@ -0,0 +1,286 @@
+package com.example.com.yan.hot.legend;
+
+import android.util.Log;
+
+import com.annimon.stream.function.Predicate;
+import com.yan.hot.legend.action.Action;
+import com.yan.hot.legend.action.Coordinate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by on 2018/8/2.
+ * 改变坐标
+ */
+public class ChangeCoordinate {
+    private static List<Action> actions = null;
+
+    public static void change(List<Action> actions) {
+        Log.e(TAG, "---------change------------");
+        ChangeCoordinate.actions = actions;
+
+//        addNew("暗之秘境-修罗","暗之秘境-魔窟");
+//        setCoordinate("暗之秘境-魔窟",941,1351,882,1463);
+
+        show("暗之秘境-魔窟");
+
+//        ActionFile.write(actions);
+    }
+
+    private static void show(String name) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            for (int i = 0; i < coordinates.size(); i++) {
+                Log.e("test", "tmp: " + coordinates.get(i) + "\n");
+            }
+            Log.e("test", "tmp: " + tmp);
+        }else {
+            Log.e("test", "tmp:  null ");
+
+        }
+    }
+
+    private static void addTime(String name, int startIndex, long addTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            for (int i = startIndex; i < coordinates.size(); i++) {
+                coordinate = coordinates.get(i);
+                coordinate.setTime(coordinate.getTime() + addTime);
+            }
+        }
+    }
+
+    private static Action getAction(String name) {
+        Action tmp = null;
+        for (Action action : actions) {
+            if (action.getName().equals(name)) {
+                tmp = action;
+                break;
+            }
+        }
+        return tmp;
+    }
+
+    private static void addTime(String name, Predicate<Coordinate> predicate, long addTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            for (int i = 0; i < coordinates.size(); i++) {
+                coordinate = coordinates.get(i);
+                if (predicate.test(coordinate)) {
+                    addTime(name, i, addTime);
+                }
+            }
+        }
+    }
+
+    private static void setTime(String name, int index, long delay) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            if (index > 0 && index < coordinates.size()) {
+                Coordinate preCoordinate = coordinates.get(index - 1);
+                Coordinate currentCoordinate = coordinates.get(index);
+                addTime(name, index, delay - (currentCoordinate.getTime() - preCoordinate.getTime()));
+            }
+        }
+    }
+
+    private static void addTimeAll(String name, long addTime) {
+        int size = getSize(name);
+        for (int i = 0; i < size; i++) {
+            addTime(name, i, addTime);
+        }
+    }
+
+    private static void setCoordinate(String name,
+                                      int oldX, int oldY, int newX, int newY) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            for (Coordinate coordinate : coordinates) {
+                if (coordinate.getX() == oldX && coordinate.getY() == oldY) {
+                    coordinate.setX(newX);
+                    coordinate.setY(newY);
+                }
+            }
+        }
+    }
+
+    private static void addCoordinateEndRepeate(String name, int newX, int newY, int offsetTime, int repeate) {
+        for (int i = 0; i < repeate; i++) {
+            addCoordinateEnd(name, newX, newY, offsetTime);
+        }
+    }
+
+    private static void addCoordinateTop(String name, int newX, int newY, int offsetTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            coordinate = new Coordinate(newX, newY);
+            coordinate.setTime(coordinates.get(0).getTime() - offsetTime);
+            coordinates.add(0, coordinate);
+        }
+    }
+
+    private static void addCoordinateEnd(String name, int newX, int newY, int offsetTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            coordinate = new Coordinate(newX, newY);
+            coordinate.setTime(coordinates.get(coordinates.size() - 1).getTime() + offsetTime);
+            coordinates.add(coordinate);
+        }
+    }
+
+    private static void addCoordinate(String name, int index, int newX, int newY, int offsetTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            coordinate = new Coordinate(newX, newY);
+            coordinate.setTime(coordinates.get(index - 1).getTime() + offsetTime);
+            coordinates.add(index, coordinate);
+            for (int i = index + 1; i < coordinates.size(); i++) {
+                coordinates.get(i).setTime(coordinates.get(i).getTime() + offsetTime);
+            }
+        }
+    }
+
+    private static void addCoordinate(String name, Predicate<Coordinate> predicate, int newX, int newY, int offsetTime) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            List<Coordinate> tmpCoordinates = new ArrayList<Coordinate>(coordinates);
+            Coordinate coordinate;
+            int offset = 0;
+            for (int i = 0; i < tmpCoordinates.size(); i++) {
+                Coordinate value = tmpCoordinates.get(i);
+                if (predicate.test(value)) {
+                    offset += 1;
+                    coordinate = new Coordinate(newX, newY);
+                    coordinate.setTime(value.getTime() + offsetTime);
+                    coordinates.add(i + offset, coordinate);
+
+                    for (int j = i + offset + 1; j < coordinates.size(); j++) {
+                        coordinates.get(j).setTime(coordinates.get(j).getTime() + offsetTime);
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private static int getSize(String name) {
+        Action tmp = getAction(name);
+        int size = 0;
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            size = coordinates.size();
+        }
+        return size;
+    }
+
+    private static void setCoordinateIndex(String name,
+                                           int startIndex, int endIndex, int newX, int newY) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Coordinate coordinate;
+            for (int i = startIndex; i <= endIndex; i++) {
+                coordinate = coordinates.get(i);
+                coordinate.setX(newX);
+                coordinate.setY(newY);
+            }
+        }
+    }
+
+    private static void addNew(String oldName, String newName) {
+        Action tmp = getAction(oldName);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            Action actionCopy = copy(tmp, newName);
+            for (int i = 0; i < coordinates.size(); i++) {
+                Log.e("test", "tmp: " + coordinates.get(i) + "\n");
+            }
+
+            actions.add(actionCopy);
+            //            ActionFile.write(actions);
+            Log.e("test", "tmp: " + actionCopy);
+        }
+    }
+
+    private static void deletePostions(String name, int... positions) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            List<Coordinate> delete = new ArrayList<Coordinate>();
+            for (int i = 0; i < coordinates.size(); i++) {
+                for (int position : positions) {
+                    if (position == i) {
+                        delete.add(coordinates.get(i));
+                        break;
+                    }
+                }
+            }
+            coordinates.removeAll(delete);
+        }
+    }
+
+    private static void deletePostion(String name, int position) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            if (position != 0) {
+                addTime(name, position,
+                        coordinates.get(position).getTime() - coordinates.get(position - 1).getTime());
+            }
+            coordinates.remove(position);
+        }
+    }
+
+    private static void delete(String name, int startIndex) {
+        Action tmp = getAction(name);
+        if (tmp != null) {
+            List<Coordinate> coordinates = tmp.getCoordinates();
+            List<Coordinate> delete = new ArrayList<Coordinate>();
+            for (int i = startIndex; i < coordinates.size(); i++) {
+                delete.add(coordinates.get(i));
+            }
+            coordinates.removeAll(delete);
+        }
+    }
+
+    private static Action copy(Action source, String name) {
+        List<Coordinate> coordinates = new ArrayList<Coordinate>();
+        for (Coordinate coordinate : source.getCoordinates()) {
+            Coordinate coordinate1;
+            if (coordinate.getX() == 911 && coordinate.getY() == 1130) {
+                coordinate1 = new Coordinate(909, 1018);
+            } else {
+                coordinate1 = new Coordinate(coordinate.getX(), coordinate.getY());
+            }
+            coordinate1.setTime(coordinate.getTime());
+            coordinates.add(coordinate1);
+        }
+        Action action = new Action();
+        action.setName(name);
+        action.setCoordinates(coordinates);
+        action.setActionTime(source.getActionTime());
+        return action;
+    }
+
+    private static boolean isSame(Coordinate value, int x, int y) {
+        return value.getX() == x && value.getY() == y;
+    }
+}
