f44bc9af2d97d700217eb6051da8e3c05c54c1e3
zhouzhaoyan <zhouzhaoyan@lifesense.com>
2019-02-05 23:22:38 +0800

    更新代码

--- com.yan.hot.legend/app/src/main/java/com/example/com/yan/hot/legend/ClickService.java
+++ com.yan.hot.legend/app/src/main/java/com/example/com/yan/hot/legend/ClickService.java
@@ -367,7 +367,7 @@ public class ClickService extends GrayService {
                                 }
                             }
                             if (nextMode == null){
-                                if (getLastSecondInDay(System.currentTimeMillis()) - System.currentTimeMillis() < 3*60*60*1000){
+                                if (getLastSecondInDay(System.currentTimeMillis()) - System.currentTimeMillis() < 6*60*60*1000){
                                     nextMode = ActionRun.ModeType.NIGHT;
                                 } else {
                                     nextMode = ActionRun.ModeType.TASK;
