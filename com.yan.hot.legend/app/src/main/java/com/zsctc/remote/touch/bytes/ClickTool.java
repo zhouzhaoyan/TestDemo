5427c5c5c093778d52a0cb493ccd3341de210964
zhouzhaoyan <zhouzhaoyan@lifesense.com>
2019-07-08 00:06:40 +0800

    更新代码

--- com.yan.hot.legend/app/src/main/java/com/zsctc/remote/touch/bytes/ClickTool.java
+++ com.yan.hot.legend/app/src/main/java/com/zsctc/remote/touch/bytes/ClickTool.java
@@ -295,6 +295,7 @@ public class ClickTool {
             case 玩蛋qq浏览器双开:
             case 火树猎豹浏览器:
             case 玩蛋猎豹浏览器:
+            case 趣头条qq浏览器双开:
                 result = true;
                 break;
         }
