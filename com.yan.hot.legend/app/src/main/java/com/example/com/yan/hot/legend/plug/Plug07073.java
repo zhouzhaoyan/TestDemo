d9295eab571fddb7e101ae60d818f029d9c70f24
zhouzhaoyan <zhouzhaoyan@lifesense.com>
2020-03-18 08:52:22 +0800

    更新代码

--- com.yan.hot.legend/app/src/main/java/com/example/com/yan/hot/legend/plug/Plug07073.java
+++ com.yan.hot.legend/app/src/main/java/com/example/com/yan/hot/legend/plug/Plug07073.java
@@ -38,14 +38,22 @@ public class Plug07073 {
 
     private List<Coordinate> getCoordinate(){
         List<Coordinate> coordinates = new ArrayList<>();
-        Coordinate coordinate = new Coordinate(990, 630);
         int timeIndex = 824085;
+
+        Coordinate coordinate = new Coordinate(990, 630);
         coordinate.setTime(timeIndex);
         coordinates.add(coordinate);
+
         coordinate = new Coordinate(530, 1230);
         timeIndex += 2000;
         coordinate.setTime(timeIndex);
         coordinates.add(coordinate);
+
+        coordinate = new Coordinate(990, 630);
+        timeIndex += 2000;
+        coordinate.setTime(timeIndex);
+        coordinates.add(coordinate);
+
         return coordinates;
     }
 
@@ -75,7 +83,7 @@ public class Plug07073 {
                 }
                 currentTime = tmp.getTime();
                 if (sleep == 0){
-                    sleep = 15000;
+                    sleep = 20000;
                 }
                 allTime += sleep;
                 clickService.runClick(sleep, tmp);
